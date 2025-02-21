8af612cf69a28700c86dad4eb3ccd3586f7305e5
gmegidish <gil@megidish.net>
2025-02-21 21:20:35 +0100

    feat: added version, timestamp, package versions, result to fuselock-report

--- src/fuselock-report.js
+++ src/fuselock-report.js
@@ -9,17 +9,48 @@
 const wrapPermissions = (permissionsModel) => {
 
 	const fs = require("fs");
+	const path = require("path");
 	const reportFile = process.env.FUSELOCK_REPORT_FILE || null;
 
 	/**
+	 * @param {string} packagePath
+	 * @returns {string}
+	 */
+	const getPackageVersion = (packagePath) => {
+		const packageJsonPath = path.join(packagePath, "package.json");
+		if (!fs.existsSync(packageJsonPath)) {
+			return "";
+		}
+
+		const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+		return packageJson.version;
+	};
+
+	/**
 	 * @param {string} name
+	 * @param {string[]} packages
+	 * @param {boolean} result
 	 * @param {object} params
 	 */
-	const report = (name, params) => {
+	const report = (name, packages, result, params) => {
+
+		const packagesWithVersions = packages.map(path => {
+			return {
+				path,
+				version: getPackageVersion(path),
+			}
+		});
+
 		if (reportFile) {
 			const item = {
-				name,
-				params,
+				"fuselock-report": {
+					version: 1,
+					timestamp: new Date().toISOString(),
+					name,
+					packages: packagesWithVersions,
+					result,
+					params,
+				},
 			};
 
 			const data = JSON.stringify(item) + "\n";
@@ -33,7 +64,7 @@ const wrapPermissions = (permissionsModel) => {
 	 */
 	const isExecAllowed = (command, packages) => {
 		const result = permissionsModel.isExecAllowed(command, packages);
-		report("exec", {command, packages, result});
+		report("exec", packages, result, {command});
 		return result;
 	};
 
@@ -43,14 +74,14 @@ const wrapPermissions = (permissionsModel) => {
 	 */
 	const isHttpRequestAllowed = (host, packages) => {
 		const result = permissionsModel.isHttpRequestAllowed(host, packages);
-		report("http", {host, packages, result});
+		report("http", packages, result, {host});
 		return result;
 	};
 
 	/** @param {string[]} packages */
 	const isFunctionConstructorAllowed = (packages) => {
 		const result = permissionsModel.isFunctionConstructorAllowed(packages);
-		report("eval", {packages, result});
+		report("eval", packages, result, {});
 		return result;
 	};
 
