2f5712a0d04d47a99abf912bfde1d07e9aa566e0
gmegidish <gil@megidish.net>
2025-02-27 23:11:33 +0100

    test: added assertions for err.code and err.errno

--- tests/child_process.test.js
+++ tests/child_process.test.js
@@ -110,6 +110,18 @@ FUSELOCK_E2E && describe('child_process', () => {
 		});
 	});
 
+	it('should handle null commands via execFile', (done) => {
+		try {
+			childProcess.execFile(null, [], (err, stdout, stderr) => {
+			});
+			done(new Error("Expected an error"));
+		} catch (e) {
+			assert.ok(e instanceof Error);
+			assert.ok(e.message.match(/The "(command|file)" argument must be of type string. Received null/));
+			done();
+		}
+	});
+
 	it('should block commands via execFile', (done) => {
 		childProcess.execFile('/bin/ls', ['-1', '/usr/bin'], (err, stdout, stderr) => {
 			assert.notEqual(err, null);
@@ -118,15 +130,58 @@ FUSELOCK_E2E && describe('child_process', () => {
 			done();
 		});
 	});
-	
+
+	it('should handle null commands via execFileSync', (done) => {
+		try {
+			childProcess.execFileSync(null);
+			done(new Error("Expected an error"));
+		} catch (e) {
+			assert.ok(e instanceof Error);
+			assert.ok(e.message.match(/The "(command|file)" argument must be of type string. Received null/));
+			done();
+		}
+	});
+	it('should block commands via execFileSync (null options)', (done) => {
+		try {
+			childProcess.execFileSync('/bin/ls', ['-l', '/usr/bin']);
+			done(new Error("Expected an error"));
+		} catch (e) {
+			assert.ok(e instanceof Error);
+			assert.ok(e.message.includes('/bin/ls ENOENT'));
+			done();
+		}
+	});
+
+	it('should block commands via execFileSync', (done) => {
+		try {
+			childProcess.execFileSync('/bin/ls', ['-l', '/usr/bin'], {});
+			done(new Error("Expected an error"));
+		} catch (e) {
+			assert.ok(e instanceof Error);
+			assert.equal(e.code, 'ENOENT');
+			assert.equal(e.errno, -2);
+			// TODO: assert path, spawnargs, syscall, stdout, stderr
+			assert.ok(e.message.includes('/bin/ls ENOENT'));
+			done();
+		}
+	});
+
 	// TODO: write a test with options.shell = true
 
 	it('should block commands via spawn', (done) => {
 		const c = childProcess.spawn('/bin/ls', ['-1', '/usr/bin']);
+		assert.notEqual(c, null);
+		assert.ok(c instanceof childProcess.ChildProcess);
+		assert.notEqual(c.stdout, null);
+		assert.notEqual(c.stderr, null);
+
 		c.on("error", (err) => {
-			// TODO: add assertions for errno == -2, code == "ENOENT"
+			// Add assertions for errno and code
 			assert.equal(err.message, "spawn /bin/ls -1 /usr/bin ENOENT");
+			assert.equal(err.code, "ENOENT");
+			assert.equal(err.errno, -2);
 			done();
 		});
 	});
+
 });
